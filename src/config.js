f3404171c73d8254facab5b5bc9d17a068504a0d
tt <tt000.work@gmail.com>
2024-07-20 03:25:35 +0530

    initial test 1.01

--- src/config.js
+++ src/config.js
@@ -70,7 +70,7 @@ function setUpParams() {
     .option('p', {
     alias: 'port',
     description: 'listening port',
-    default: 7500
+    default: 8000
     })
     .option('k', {
     alias: 'key',
